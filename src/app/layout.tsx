54e675fe3d5db56d57800a198d680ff175b577ff
Sebastian Contreras <sebconrios@gmail.com>
2026-02-16 19:18:12 +0100

    Fix favicon using app/icon.png

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,10 +18,6 @@ export const metadata: Metadata = {
   verification: {
     google: 'Lm-BZKXzWTnZot2QTvqY-ltfunMHEwwuxOfg7Ss9RWc',
   },
-  icons: {
-    icon: '/favicon.png',
-    apple: '/favicon.png',
-  },
 };
 
 export default function RootLayout({
